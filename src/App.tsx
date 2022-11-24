5b5bad78de1e7e7cb0338bdd78c75c2b058ecf3f
Amin Beterbekow <betteramsly>
2022-11-24 23:08:05 +0300

    убрал лишнее

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Contacts } from './components/contacts/Contacts'
 import { Login } from './components/login/Login'
